b21f3c21c070f4f2ba68476f999c25fc0447e5fa
shorenko <serhii.horenko@gmail.com>
2026-09-15 16:15:28 +0200

    initial setup of the project

---
+++ CoolblueSHorenko/CoolblueSHorenko/CoolblueSHorenkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolblueSHorenkoApp.swift
+//  CoolblueSHorenko
+//
+//  Created by Serhii Horenko on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct CoolblueSHorenkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoolblueSHorenko/CoolblueSHorenkoTests/CoolblueSHorenkoTests.swift
@@ -0,0 +1,18 @@
+//
+//  CoolblueSHorenkoTests.swift
+//  CoolblueSHorenkoTests
+//
+//  Created by Serhii Horenko on 15/09/2026.
+//
+
+import XCTest
+
+@testable import CoolblueSHorenko
+
+final class CoolblueSHorenkoTests: XCTestCase {
+
+    // Placeholder so the target has at least one test; replace as features land.
+    func testTargetIsLinked() throws {
+        XCTAssertFalse(String(describing: ContentView.self).isEmpty)
+    }
+}
